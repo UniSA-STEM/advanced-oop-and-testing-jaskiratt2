bd8760c5f0f37c9428ac3d25c54a78c54f25717d
jaskiratt2 <uppjy001@mymail.unisa.edu.au>
2025-11-23 13:33:31 +1030

    made a separate file for reptile to define class and satisfy the needs.

---
+++ reptile.py
@@ -0,0 +1,8 @@
+from animal import Animal
+
+class Reptile(Animal):
+    def __init__(self, name, species, age, dietary_needs):
+        super().__init__(name, species, age, dietary_needs)
+
+    def make_sound(self):
+        return f"{self.get_name()} hisses"
