a13823261a65b97a90343684817b4c2d191f4238
jaskiratt2 <uppjy001@mymail.unisa.edu.au>
2025-11-24 14:54:33 +1030

    made staff class and added name, role and many more to ensure the staff duties.

--- staff.py
+++ staff.py
@@ -6,3 +6,20 @@ ID: 110426141
 Username: uppjy001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+class Staff:
+    def _init__ (self,name,role):
+        if name == "":
+            print("Error: Sorry! There should be a staff name everytime")
+            return
+        self.__name = name
+        self.__role = role
+
+    def get_name(self):
+        return self.name
+    def get_role(self):
+        return self.role
+    def work(self):
+        return self.__name + " works as " + self.__role
+    def __str__(self):
+        return self.__name + " (" + self.__role + ")"
